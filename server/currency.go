e09ffb53a47c1e2cfad8103e3758aad44db17c59
ilies <iliesassaddev@gmail.com>
2022-12-14 13:23:35 +0100

    first commit

---
+++ server/currency.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"context"
+	"currency/protos/currency"
+
+	"github.com/hashicorp/go-hclog"
+)
+
+type Currency struct {
+	log hclog.Logger
+	currency.UnimplementedCurrencyServer
+}
+
+func NewCurrency(l hclog.Logger) *Currency {
+	return &Currency{l, currency.UnimplementedCurrencyServer{}}
+}
+
+func (c *Currency) GetRate(ctx context.Context, rr *currency.RateRequest) (*currency.RateResponse, error) {
+	c.log.Info("Handle GetRate", "base", rr.GetBase(), "destination", rr.GetDestination())
+
+	return &currency.RateResponse{Rate: 0.5}, nil
+}
